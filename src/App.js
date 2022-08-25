eb332cf8afe0c3f54b40842ed168f2c862bce12e
RYAN DOWNS <rdowns@us.ibm.com>
2022-08-25 11:16:18 -0700

    tying to get my app.js to render

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { useState, useEffect, Link } from "react";
+import Axios from "axios";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import AddPlant from "./components/AddPlant";
+import { Homepage } from "./pages/Homepage";
+import { Plant } from "./pages/Plant";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
 class LambdaDemo extends Component {
   constructor(props) {
@@ -35,20 +43,42 @@ class LambdaDemo extends Component {
   }
 }
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            AAAAAAH <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
-      </div>
+function App() {
+  //setting a const that loads on launch
+  const [listOfPlants, setListOfPlants] = useState([]);
+
+  //contact the db using axios, then set above const to the server's response
+  useEffect(() => {
+    Axios.get("https://brents-plant-trackr.herokuapp.com/getPlants").then(
+      (response) => {
+        setListOfPlants(response.data);
+      }
     );
-  }
+  }, []);
+
+  return (
+    <div className="App">
+      <Router>
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Brand href="/">Plant Trackr</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="/addplant">Add plant</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/addplant" element={<AddPlant />} />
+          <Route path="*" element={<h1>Page not found</h1>} />
+          <Route path="/plant/:plant_id" element={<Plant />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;

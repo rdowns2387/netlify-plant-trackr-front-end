d38da24b72b49de036c7eaf194b76fa322b71471
RYAN DOWNS <rdowns@us.ibm.com>
2022-08-25 11:20:42 -0700

    comment out lambda demo

--- src/App.js
+++ src/App.js
@@ -10,38 +10,38 @@ import { Homepage } from "./pages/Homepage";
 import { Plant } from "./pages/Plant";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { loading: false, msg: null };
-  }
+// class LambdaDemo extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { loading: false, msg: null };
+//   }
 
-  handleClick = (api) => (e) => {
-    e.preventDefault();
+//   handleClick = (api) => (e) => {
+//     e.preventDefault();
 
-    this.setState({ loading: true });
-    fetch("/.netlify/functions/" + api)
-      .then((response) => response.json())
-      .then((json) => this.setState({ loading: false, msg: json.msg }));
-  };
+//     this.setState({ loading: true });
+//     fetch("/.netlify/functions/" + api)
+//       .then((response) => response.json())
+//       .then((json) => this.setState({ loading: false, msg: json.msg }));
+//   };
 
-  render() {
-    const { loading, msg } = this.state;
+//   render() {
+//     const { loading, msg } = this.state;
 
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>
-          {loading ? "Loading..." : "Call Lambda"}
-        </button>
-        <button onClick={this.handleClick("async-dadjoke")}>
-          {loading ? "Loading..." : "Call Async Lambda"}
-        </button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    );
-  }
-}
+//     return (
+//       <p>
+//         <button onClick={this.handleClick("hello")}>
+//           {loading ? "Loading..." : "Call Lambda"}
+//         </button>
+//         <button onClick={this.handleClick("async-dadjoke")}>
+//           {loading ? "Loading..." : "Call Async Lambda"}
+//         </button>
+//         <br />
+//         <span>{msg}</span>
+//       </p>
+//     );
+//   }
+// }
 
 function App() {
   //setting a const that loads on launch
